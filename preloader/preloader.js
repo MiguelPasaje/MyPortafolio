6180f4f07c8b14defb5e6c596e67b21f07276138
MiguelPasaje <cartel-dy@hotmail.com>
2022-02-24 10:33:35 -0500

    tiempo en el preloader

--- preloader/preloader.js
+++ preloader/preloader.js
@@ -1,5 +1,6 @@
 /* accion que carga todo como imagenes, estilos y no solo el html */
+/* el .ready solo carga el html */
 window.onload = function () {
-    $('#preloader').fadeOut();
+    $('#preloader').fadeOut(3000);
     $('body').removeClass('hidden');
 }
