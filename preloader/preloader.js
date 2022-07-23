df97b37ecbf13db34a535047fda985d4290ec8ee
MiguelPasaje <cartel-dy@hotmail.com>
2022-07-22 21:56:30 -0500

    se agregan elementos animaciones y diseño

--- preloader/preloader.js
+++ preloader/preloader.js
@@ -1,6 +1,6 @@
 /* accion que carga todo como imagenes, estilos y no solo el html */
 /* el .ready solo carga el html */
 window.onload = function () {
-    $('#preloader').fadeOut(3000);
+    $('#preloader').fadeOut(1000);
     $('body').removeClass('hidden');
 }
